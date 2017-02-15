f8bb6b31d324c044bcd569a808f525996470a715
郭若辰 <guoruochen@ycsjjqr.com>
2017-02-15 23:02:05 +0800

    Merge branch 'master' of https://github.com/guoruochen/JavaSE
    # Conflicts:
    #	GitTest/src/GitTest.java

--- GitTest/src/GitTest.java
+++ GitTest/src/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git Again");
-        System.out.println("hehe")
+        System.out.println("hehe");
         System.out.println("hehehehe");
     }
 }
