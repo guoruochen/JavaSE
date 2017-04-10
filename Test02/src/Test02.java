d4fe9c310f2c852f2605a9e415ea7a00ca0e317c
郭若辰 <111>
2017-04-10 16:30:27 +0800

    Test02：强转的面试要点

---
+++ Test02/src/Test02.java
@@ -0,0 +1,13 @@
+/**
+ * @author 郭若辰
+ * @create 2017-04-10 16:23
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        byte b = 1 + 2;//由于编译器确定1+2一定在byte的范围内，所以自动进行了强制类型转换，可以通过编译
+        byte b1 = 1;
+        //!b = b1 + 1;b1+1的结果是int，由于b1是变量，编译器无法判断b1+1是否在byte范围内，所以不能通过编译
+        byte b2 = 2;
+        //!b = b1 + b2;b1 + b2的结果是int，由于b1, b2都是是变量，编译器无法判断b1 + b2是否在byte范围内，所以不能通过编译
+    }
+}
