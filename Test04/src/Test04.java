0ee8e81ee371c052e2559a74033d77135d59525a
郭若辰 <111>
2017-04-11 15:06:13 +0800

    算术运算符、使用"+"拼接字符串、三元运算符

--- Test04/src/Test04.java
+++ Test04/src/Test04.java
@@ -15,12 +15,10 @@ public class Test04 {
         double d = 25.5;
         System.out.println("加：" + (i + d) + "\n减：" + (i - d) + "\n乘：" + (i * d) + "\n除：" + (i / d) + "\n取模：" + (i % d));
         Scanner s = new Scanner(System.in);
-        System.out.print("请输入一个整数：");
-        int a = s.nextInt();
-        System.out.println((a % 2 == 0) ? "您输入的是偶数" : "您输入的是奇数");
-        System.out.print("请再输入任意数:");
+        System.out.print("请输入任意整数：");
         double b = s.nextDouble();
-        System.out.println((b % 1 == 0) ? "您输入的是整数" : "您输入的是小数");
+        System.out.println((b % 1 != 0) ? "您输入的是小数" : (b % 2 == 0) ? "您输入的是偶数" : "您输入的是奇数");
+        s.close();
     }
 
     private static void stringConcat() {
