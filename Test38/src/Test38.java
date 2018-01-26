b81035978d0b0dac958bb19d78aec2f91977fb9a
郭若辰 <111>
2018-01-26 17:38:13 +0800

    排序

--- Test38/src/Test38.java
+++ Test38/src/Test38.java
@@ -15,13 +15,16 @@ public class Test38 {
             int[] arr_1 = generateRandomArray(size, value);
             int[] arr_2 = copyArray(arr_1);
             int[] arr_3 = copyArray(arr_1);
-            if (!isEqual(recursive(arr_1, 0, arr_1.length - 1), rightMethod(arr_2))) {
+            if (!isEqual(insert(arr_1), rightMethod(arr_2))) {
                 success = false;
                 System.out.println("错误用例：" + Arrays.toString(arr_3));
                 break;
             }
         }
         System.out.println(success ? "算法正确" : "算法错误");
+
+        int[] arr = {1, 2, 32, 54, 66, 32, 55, 2, 5, 6, 7, 34, 27};
+        System.out.println(Arrays.toString(heap(arr)));
     }
 
     //冒泡排序第一种写法
@@ -112,6 +115,76 @@ public class Test38 {
         //将help数组写回原数组的对应位置
         for (i = 0; i < help.length; i++) {
             arr[L + i] = help[i];
+        }
+    }
+
+    //随机快速排序
+    //随机一个元素为划分值，将数组分为小于区、等于区和大于区
+    //经典快速排序使用最后一个元素为划分值，经典快排有最好情况O(N)和O(N^2)
+    //随机快排时间复杂度O(N*logN)，原理与归并排序相同，额外空间复杂度O(logN)，是不稳定排序
+    private static int[] quick(int[] arr, int L, int R) {
+        if (L < R) {
+            swap(arr, L + (int) (Math.random() * (R - L + 1)), R);//随机选一个位置的元素，与数组最后一个元素交换，作为划分值
+            int[] p = partition(arr, L, R);
+            quick(arr, L, p[0] - 1);//将小于区再划分
+            quick(arr, p[1] + 1, R);//将大于区再划分
+        }
+        return arr;
+    }
+
+    private static int[] partition(int[] arr, int L, int R) {
+        int less = L - 1;//小于区起始位置
+        int more = R;//大于区起始位置
+        while (L < more) {
+            if (arr[L] < arr[R]) {//如果当前指针的元素小于划分值
+                swap(arr, ++less, L++);//就将当前指针的元素与小于区右边的元素交换，小于区向右扩，当前指针也向右移一位
+            } else if (arr[L] > arr[R]) {//如果当前指针的元素大于划分值
+                swap(arr, --more, L);//就将当前指针的元素与大于区左边的元素交换，大于区向左扩，当前指针不变
+            } else {//如果当前指针的元素等于划分值
+                L++;//只将当前指针右移一位
+            }
+        }
+        swap(arr, more, R);//由于大于区最后一个元素是划分值，要将大于区第一个元素与划分值交换
+        return new int[]{less + 1, more};//返回等于区的起始位置和结束位置
+    }
+
+    //堆排序
+    //将数组想象为完全二叉树，左子节点2i+1，右子节点2i+2，父节点(i-1)/2
+    //第一步：建立大根堆结构（根节点大于等于子树所有值）--->往上走
+    //第二步：将大根堆第一个和最后一个交换，依次去掉最后一个，再从第一个往子节点比 --->往下走
+    //建立大根堆时间复杂度O(N)
+    //堆排序时间复杂度O(N*logN)，原理与归并排序相同，额外空间复杂度O(1)，是不稳定排序
+    private static int[] heap(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {//第一步：建立大根堆结构
+            heapInsert(arr, i);
+        }
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);//完成大根堆结构建立之后，第一个一定是最大的，交换到数组最后一个，然后堆长度减一
+        while (heapSize > 0) {//循环进行从高阶树比到低阶树（实际上还是建立大根堆结构）
+            heapify(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+        }
+        return arr;
+    }
+
+    private static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {//和父节点比大小，如果比父节点大就交换
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    private static void heapify(int[] arr, int index, int size) {
+        int left = index * 2 + 1;//左子节点
+        while (left < size) {
+            int largest = left + 1 < size && arr[left + 1] > arr[left] ? left + 1 : left;//返回子节点值较大的下标
+            largest = arr[largest] > arr[index] ? largest : index;//返回当前节点和子节点值较大的下标
+            if (largest == index) {//如果发现还是当前节点最大，结束遍历
+                break;
+            }
+            swap(arr, largest, index);//如果某个子节点比当前节点大，将两者交换
+            index = largest;
+            left = index * 2 + 1;
         }
     }
 
